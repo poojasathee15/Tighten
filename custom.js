47f97352b5843867aef47017281227cc9a66c03d
pooja sathe <poojasathee@gmail.com>
2022-02-25 18:11:57 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,34 @@
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+var el = document.getElementById('menu-toggle');
+if(el){
+  el.addEventListener('click', () => {
+    menu.classList.toggle('hidden');  
+  });
+}  
+
+// Swiper: Slider
+    new Swiper('.swiper-container', {
+        loop: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 20,
+        breakpoints: {
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1028: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
+
