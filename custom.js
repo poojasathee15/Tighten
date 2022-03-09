57de4606fcc42672c26b0734ac9c512222913fe1
pooja sathe <poojasathee@gmail.com>
2022-03-09 14:58:16 +0530

    ui-final-changes

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('#menu-button');
+//const button = document.querySelector('#menu-button');
 const menu = document.querySelector('#menu');
 
 var el = document.getElementById('menu-toggle');
@@ -7,28 +7,28 @@ if(el){
     menu.classList.toggle('hidden');  
   });
 }  
-
 // Swiper: Slider
-    new Swiper('.swiper-container', {
-        loop: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 3,
-        paginationClickable: true,
-        spaceBetween: 20,
-        breakpoints: {
-            1920: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-            1028: {
-                slidesPerView: 2,
-                spaceBetween: 30
-            },
-            480: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            }
+new Swiper('.swiper-container', {
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 20,
+    breakpoints: {
+        1920: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1028: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 10
         }
-    });
+    }
+});
+
 
